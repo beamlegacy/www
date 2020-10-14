0702af4f2a855ad5e4cb9a470faba9e3a35e438b
Jimmy Oliger <jimmyoliger@gmail.com>
2020-10-14 17:01:14 +0200

    Add meta

--- posthtml.config.js
+++ posthtml.config.js
@@ -3,6 +3,10 @@ module.exports = {
     "posthtml-expressions": {
       locals: {
         title: "Beam",
+        description:
+          "Comming soon to your Mac, a new way to collect your thoughts and experience the internet.",
+        url: "https://beamapp.co/",
+        twitter: "getonbeam",
         year: new Date().getFullYear(),
       },
     },
