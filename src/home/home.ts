4054bc925819decafb158bc8467018f009a77151
Mathieu Jouhet <mathieu@beamapp.co>
2022-03-22 16:45:32 +0100

    Tweak scroll animation and toggleMode at the end

--- src/home/home.ts
+++ src/home/home.ts
@@ -1,3 +1,5 @@
+import {BeamWindow} from "home/beam-window/BeamWindow"
+
 export class Homepage {
   private observer: IntersectionObserver
 
@@ -16,16 +18,24 @@ export class Homepage {
       if (ratio >= min && ratio <= max) {
         const mappedRatio = map(ratio, min, max, 0, 1)
         title?.style.setProperty("opacity", `${mappedRatio}`)
-        title?.style.setProperty("transform", `translateY(${2 * (1 - mappedRatio)}em)`)
+        title?.style.setProperty("transform", `translateY(${3 * (1 - mappedRatio)}em)`)
         hero?.style.setProperty("opacity", `${1 - mappedRatio}`)
         hero?.style.setProperty("transform", `scale(${1 - mappedRatio})`)
       } else {
         title?.style.setProperty("opacity", ratio < max ? "0" : "1")
-        title?.style.setProperty("transform", `translateY(${ratio < max ? "2" : "0"}em)`)
+        title?.style.setProperty("transform", `translateY(${ratio < max ? "3" : "0"}em)`)
         hero?.style.setProperty("opacity", ratio < max ? "1" : "0")
         hero?.style.setProperty("transform", `scale(${ratio < max ? "1" : "0"})`)
+
+        const window = document.querySelector("beam-window") as BeamWindow
+        if (ratio >= 1 && window.url.startsWith("web/")) {
+          window?.toggleMode()
+        }
       }
-    }, {threshold: new Array(100).fill(0).map((v, i) => i / 100)})
+    }, {
+      rootMargin: "0px 0px 32px 0px",
+      threshold: new Array(100).fill(0).map((v, i) => (i + 1) / 100)
+    })
     if (demo) {
       this.observer.observe(demo)
     }
