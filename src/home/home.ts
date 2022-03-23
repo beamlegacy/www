1ee54f343d3672ca044bd163a73a4dd6f1d5b2cf
Mathieu Jouhet <mathieu@beamapp.co>
2022-03-23 12:13:07 +0100

    Basic reset of animation when scrolling back

--- src/home/home.ts
+++ src/home/home.ts
@@ -1,7 +1,10 @@
-import {BeamWindow} from "home/beam-window/BeamWindow"
+import {BeamWindow, BeamWindowMode} from "home/beam-window/BeamWindow"
+import {BeamWindowAnimation} from "home/BeamWindowAnimation"
 
 export class Homepage {
   private observer: IntersectionObserver
+  private animation: BeamWindowAnimation
+  private timeout: ReturnType<typeof setTimeout> | undefined
 
   constructor() {
     this.animation = new BeamWindowAnimation()
@@ -15,22 +18,52 @@ export class Homepage {
       const ratio = entries[0].intersectionRatio
       const min = 0.4
       const max = 0.8
-      const title = demo?.querySelector(".title") as HTMLElement
+      let title = demo?.querySelector(".title") as HTMLElement
+
       if (ratio >= min && ratio <= max) {
         const mappedRatio = map(ratio, min, max, 0, 1)
-        title?.style.setProperty("opacity", `${mappedRatio}`)
-        title?.style.setProperty("transform", `translateY(${4 * (1 - mappedRatio)}em)`)
         hero?.style.setProperty("opacity", `${1 - mappedRatio}`)
         hero?.style.setProperty("transform", `scale(${1 - mappedRatio})`)
       } else {
-        title?.style.setProperty("opacity", ratio < max ? "0" : "1")
-        title?.style.setProperty("transform", `translateY(${ratio < max ? "4" : "0"}em)`)
         hero?.style.setProperty("opacity", ratio < max ? "1" : "0")
         hero?.style.setProperty("transform", `scale(${ratio < max ? "1" : "0"})`)
 
-        const window = document.querySelector("beam-window") as BeamWindow
-        if (ratio >= .99 && window.url.startsWith("web/")) {
-          setTimeout(() => window?.toggleMode(), 500)
+        const win = document.querySelector("beam-window") as BeamWindow
+        if (ratio >= .99) {
+          title?.style.setProperty("opacity", "1")
+          title?.classList.add("in")
+          const strong = title?.querySelector("strong")
+          strong?.classList.add("in")
+          if (win && win.mode === BeamWindowMode.web) {
+            this.clearTimeout()
+            this.timeout = setTimeout(() => {
+              this.timeout = setTimeout(() => {
+                win.mode = BeamWindowMode.writing
+              }, 1000)
+            }, 500)
+          }
+        } else if (ratio < .4) {
+          this.clearTimeout()
+          this.animation.cancelAnimation()
+          this.animation.switches = 0
+          win.mode = BeamWindowMode.web
+          this.animation.cancelAnimation()
+          this.animation.switches = 0
+          if (title) {
+            const messages = (window as any).messages
+            const msg = messages.demo.title
+            this.animation.changeTitle(msg, true)
+            title = demo?.querySelector(".title") as HTMLElement
+            title.classList.remove("in")
+            const strong = title?.querySelector("strong")
+            strong?.classList.remove("in")
+            title.style.setProperty("opacity", "0")
+          }
+        } else {
+          // const mappedRatio = map(ratio, .4, max, 0, 1)
+          // console.log(mappedRatio, title)
+          // title?.style.setProperty("opacity", ratio < max ? `${mappedRatio}` : "1")
+          // title?.style.setProperty("transform", `translateY(${ratio < max ? 4 * mappedRatio : 0}em)`)
         }
       }
 
@@ -54,6 +87,11 @@ export class Homepage {
       this.observer.observe(demo)
     }
   }
+
+  clearTimeout = (): void => {
+    this.timeout && clearTimeout(this.timeout)
+  }
+
 }
 
 new Homepage()
