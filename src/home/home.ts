d6d5121a13328e8810b42a7abf1b33241b642079
Mathieu Jouhet <mathieu@beamapp.co>
2022-03-23 16:13:39 +0100

    Tweak gradient scroll response

--- src/home/home.ts
+++ src/home/home.ts
@@ -82,11 +82,11 @@ export class Homepage {
       this.updateTitleContainer(mappedRatio2)
 
       const mappedRatio3 = NumberUtil.map(entries[0].intersectionRatio, 0.4, 1)
-      const adjusted = (mappedRatio3 <= 0.5 ? mappedRatio3 : 1 - mappedRatio3) * 2
+      const adjusted = Math.sqrt((mappedRatio3 <= 0.5 ? mappedRatio3 : 1 - mappedRatio3) * 2)
       document.body.style.setProperty("--gradient-opacity", `${0.2 + adjusted * 0.2}`)
       document.body.style.setProperty("--gradient-grow", `${adjusted * 0.2 * 100}%`)
     }, {
-      rootMargin: "-84px 0px -84px 0px",
+      rootMargin: "-84px 0px -200px 0px",
       threshold: new Array(100).fill(0).map((v, i) => (i + 1) / 100)
     })
     if (demo) {
