4ef02f6705972ae636c93d30da32a4c866035203
Mathieu Jouhet <mathieu@beamapp.co>
2022-03-28 17:54:45 +0200

    Add new intersection observer for footer

--- src/home/home.ts
+++ src/home/home.ts
@@ -3,58 +3,77 @@ import {BeamWindowAnimation} from "home/BeamWindowAnimation"
 import {NumberUtil} from "util/NumberUtil"
 
 export class Homepage {
-  private observer: IntersectionObserver
+  private observer: IntersectionObserver | undefined
   private animation: BeamWindowAnimation
   private timeout: ReturnType<typeof setTimeout> | undefined
   private readonly win: BeamWindow
+  private footerObserver: IntersectionObserver | undefined
 
   constructor() {
     this.animation = new BeamWindowAnimation()
     const demo = this.demo
     this.win = document.querySelector("beam-window") as BeamWindow
-
-    this.observer = new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
-      const ratio = entries[0].intersectionRatio
-      const min = 0.4
-      const max = 0.8
-      const titleMin = 0.5
-
-      const mappedRatio = NumberUtil.map(ratio, min, max)
-      this.updateHero(1 - mappedRatio)
-
-      if (ratio <= 0.9) {
-        this.clearTimeout()
-        this.resetAnimation(false)
-      }
-
-      if (mappedRatio === 1) {
-        this.handleFullyVisible()
-      } else if (mappedRatio === 0) {
-        this.handleFullyOut()
-      }
-
-      const mappedRatio2 = NumberUtil.map(ratio, titleMin, max, 0, 1)
-      this.updateTitleContainer(mappedRatio2)
-
-      const mappedRatio3 = entries[0].intersectionRatio
-      const r = (mappedRatio3 <= 0.5 ? mappedRatio3 : 1 - mappedRatio3) * 2
-      const adjusted = Math.sqrt(r < 0.5 ? (r * 2) ** 2 * 0.5 : r)
-      document.body.style.setProperty("--gradient-opacity", `${0.2 + adjusted * 0.2}`)
-      document.body.style.setProperty("--gradient-grow", `${adjusted * 0.35 * 100}%`)
-
-      const mappedRatio4 = NumberUtil.map(ratio, 0.25, 0.65)
-      this.updateWin(mappedRatio4)
-    }, {
-      rootMargin: "0px 0px -100px 0px",
-      threshold: new Array(1000).fill(0).map((v, i) => (i + 1) / 1000)
-    })
     if (demo) {
+      this.observer = new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
+        const ratio = entries[0].intersectionRatio
+        const min = 0.4
+        const max = 0.8
+        const titleMin = 0.5
+
+        const mappedRatio = NumberUtil.map(ratio, min, max)
+        this.updateHero(1 - mappedRatio)
+
+        if (ratio <= 0.9) {
+          this.clearTimeout()
+          this.resetAnimation(false)
+        }
+
+        if (mappedRatio === 1) {
+          this.handleFullyVisible()
+        } else if (mappedRatio === 0) {
+          this.handleFullyOut()
+        }
+
+        const mappedRatio2 = NumberUtil.map(ratio, titleMin, max, 0, 1)
+        this.updateTitleContainer(mappedRatio2)
+
+        const mappedRatio3 = entries[0].intersectionRatio
+        const r = (mappedRatio3 <= 0.5 ? mappedRatio3 : 1 - mappedRatio3) * 2
+        const adjusted = Math.sqrt(r < 0.5 ? (r * 2) ** 2 * 0.5 : r)
+        document.body.style.setProperty("--gradient-opacity", `${0.2 + adjusted * 0.2}`)
+        document.body.style.setProperty("--gradient-grow", `${adjusted * 0.35 * 100}%`)
+
+        const mappedRatio4 = NumberUtil.map(ratio, 0.25, 0.65)
+        this.updateWin(mappedRatio4)
+      }, {
+        rootMargin: "0px 0px -100px 0px",
+        threshold: new Array(1000).fill(0).map((v, i) => (i + 1) / 1000)
+      })
       this.observer.observe(demo)
+    }
+
+    const footer = this.footer
+    if (footer) {
+      this.footerObserver = new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
+        const ratio = entries[0].intersectionRatio
+        const mapped = NumberUtil.map(ratio, 0, 1)
+        const content = footer.querySelector(".content") as HTMLElement
+        content?.style.setProperty("transform", `scale(${mapped})`)
+        content?.style.setProperty("opacity", `${mapped}`)
+      }, {
+        rootMargin: "0px 0px -25px 0px",
+        threshold: new Array(1000).fill(0).map((v, i) => (i + 1) / 1000)
+      })
+      this.footerObserver.observe(footer)
     }
   }
 
   get demo(): HTMLElement | null {
     return document.querySelector(".demo")
+  }
+
+  get footer(): HTMLElement | null {
+    return document.querySelector(".beam-site > footer")
   }
 
   get title(): HTMLElement | null {
