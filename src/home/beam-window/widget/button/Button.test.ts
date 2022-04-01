e152c5bb8c3da6af4c8dc13b16570f870eb234f8
Mathieu Jouhet <mathieu@beamapp.co>
2022-04-01 15:11:37 +0200

    Add test for creating button with text content

--- src/home/beam-window/widget/button/Button.test.ts
+++ src/home/beam-window/widget/button/Button.test.ts
@@ -6,4 +6,13 @@ describe("button", () => {
     const rendered = button.render()
     expect(rendered instanceof HTMLButtonElement).toBe(true)
   })
+
+  test("Creating a button with text content", () => {
+    const button = new Button()
+    const rendered = button.render()
+    button.content = "test"
+    expect(rendered instanceof HTMLButtonElement).toBe(true)
+    expect(rendered.childNodes[0].nodeType).toBe(Node.TEXT_NODE)
+    expect(rendered.textContent).toBe("test")
+  })
 })
