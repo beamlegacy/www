cd7f0f3ffe693ab6569abfde75ddff7c8457d4e1
Mathieu Jouhet <mathieu@beamapp.co>
2022-04-01 18:18:21 +0200

    Update test name

--- src/util/NumberUtil.test.ts
+++ src/util/NumberUtil.test.ts
@@ -2,7 +2,7 @@ import {NumberUtil} from "util/NumberUtil"
 
 describe("NumberUtil", () => {
   describe("NumberUtil.clamp", () => {
-    test("Clamping a value within range returns the same value", () => {
+    test("Clamping a value within bounds returns the same value", () => {
       expect(NumberUtil.clamp(0, 0, 100)).toBe(0)
       expect(NumberUtil.clamp(50, 0, 100)).toBe(50)
       expect(NumberUtil.clamp(100, 0, 100)).toBe(100)
