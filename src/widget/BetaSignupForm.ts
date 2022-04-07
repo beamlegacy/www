d7be9e64ef684fbed54e513a018f6e668748c90d
Mathieu Jouhet <mathieu@beamapp.co>
2022-04-07 18:59:14 +0200

    Fix content-type typo

--- src/widget/BetaSignupForm.ts
+++ src/widget/BetaSignupForm.ts
@@ -64,7 +64,7 @@ export class BetaSignupForm {
       method: "POST",
       mode: "no-cors",
       headers: {
-        "Content-Type": "application/json"
+        "Content-type": "application/json"
       },
       body: JSON.stringify({email})
     })
